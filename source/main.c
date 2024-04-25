d31c8390ffb3f32535ac25a4ebc123b7e50a99c3
PardoAlex0 <TAE2024.3@cinvestav.mx>
2024-04-24 19:25:42 -0600

    First Commit

---
+++ source/main.c
@@ -0,0 +1,40 @@
+/**
+ * @file    OneLed.c
+ * @brief   Application entry point.
+ */
+/**Standard input/output to debug console*/
+#include <stdio.h>
+/**Standard integer definition provided by the compiler*/
+#include <stdint.h>
+#include "MK64F12.h"
+
+
+int main(void) {
+
+	SIM->SCGC5 = 0x00000400;
+
+	PORTB->PCR[22] = 0x00000100;
+
+	GPIOB->PDOR = 0x00400000;
+
+	GPIOB->PDDR = 0x00400000;
+
+
+    while(1) {
+
+		GPIOB->PDOR = 0;
+
+		printf("RED LED ON\n");
+
+
+		GPIOB->PDOR = 0x00400000;
+
+		GPIOB->PDOR = 0x00000000;
+
+		GPIOB->PDOR = 0x00400000;
+
+		printf("RED LED OFF\n");
+
+    }
+    return 0 ;
+}
